f325b222dd9a87bd0e3987547e0cf885fbd10a11
XanderChua <72645612+XanderChua@users.noreply.github.com>
2021-09-17 18:31:04 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LibraryBorrowBookCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> StudentInLibrary = new List<Student>();
+            List<Book> BookInLibrary = new List<Book>();
+            int borrowCount = 0;
+            bool loop = true;
+            while (loop)
+            {
+                Console.WriteLine("---Library Management System---");
+                Console.WriteLine("1. Borrow books");
+                Console.WriteLine("2. Amount of books borrowed by students");
+                Console.WriteLine("3. Exit");
+
+                int input = Int32.Parse(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Enter student ID:");
+                            int studentId = Int32.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter student name:");
+                            string studentname = Console.ReadLine();
+                            //Console.WriteLine("\n");
+                            Student studentObj = new Student();
+                            studentObj.student(studentId, studentname);
+                            StudentInLibrary.Add(studentObj);
+                            borrowCount++;
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Total borrowed books: " + borrowCount + "\n");
+                            Console.WriteLine("Students who borrowed books \n");
+                            foreach(Student student in StudentInLibrary)
+                            {
+                                student.getStudentDetails();
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            loop = false;
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryBorrowBookCount
+{
+    class Student
+    {
+        public int studentId;
+        public string studentName;
+
+        public void student(int id, string name)
+        {
+            studentId = id;
+            studentName = name;
+        }
+
+        public void getStudentDetails()
+        {
+            Console.WriteLine("Student ID: " + studentId);
+            Console.WriteLine("Student Name: " + studentName + "\n");
+        }
+    }
+}
